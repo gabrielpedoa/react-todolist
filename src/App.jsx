091467c30bd368fb6ff89b9743a995d16059de5d
Gabriel Antunes <gabrielpedoa@gmail.com>
2023-10-13 21:39:41 -0300

    new project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <Global />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/styles/Global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+    *{
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+`
